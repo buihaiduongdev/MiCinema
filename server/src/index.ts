afe26279bda9a58bba4f6cf4243b8186cb34a502
buihaiduongdev <haiduongtinhoc@gmail.com>
2026-03-25 13:49:51 +0700

    chore: clear duplicate import

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,16 +7,11 @@
  * 4. Kết nối MongoDB → app.listen(PORT)
  * 5. Bắt unhandled errors
  */
-import { setServers } from 'node:dns/promises';
-
-setServers(['1.1.1.1', '8.8.8.8']);
 import { env } from './config/env';
 import { connectDB } from './config/database';
 import app from './app';
-// them moi
 import { setServers } from 'node:dns/promises';
 
-// them moi
 setServers(['1.1.1.1', '8.8.8.8']);
 const startServer = async () => {
   try {
